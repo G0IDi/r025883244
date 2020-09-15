a0f9d181930a83a5bb32f3b537fb58cd447c7513
Andres <afgoodoy@misena.edu.co>
2020-09-15 16:27:56 -0500

    commit1bra2

--- js/loljs.js
+++ js/loljs.js
@@ -1,3 +1,6 @@
 console.log("HOLA!!!!");
 console.log("supp??!!!!");
 console.log("do u like soup??!!!!");
+console.log("HOLA!!!!");
+console.log("supp??!!!!");
+console.log("do u like soup??!!!!");
